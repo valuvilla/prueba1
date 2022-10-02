c2281a700aea1843d5c624f3e900b78b98001e3b
valuvilla <113980834+valuvilla@users.noreply.github.com>
2022-10-02 08:43:09 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def convert(number):
+    num=str(number)
+    res=''
+    if number % 3 ==0:
+        res+='Pling'
+    if number % 5 == 0:
+        res+='Plang'
+    if number % 7 == 0:
+        res+= 'Plong'
+    return res if res else num
